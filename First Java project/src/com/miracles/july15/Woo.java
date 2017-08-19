b7d58749e04c580924b15b2f871e1f67bc9a4994
Miracles Software Laboratories <amit13johri@gamil.com>
2017-08-20 01:51:21 +0530

    Whole codes created till now

---
+++ First Java project/src/com/miracles/july15/Woo.java
@@ -0,0 +1,11 @@
+package com.miracles.july15;
+//Q1 Implements var-args
+public class Woo {
+	public static void main(String[] args) {
+		dostuff(1);
+		dostuff(1,2);
+	}
+	static void dostuff(int... doArgs) {
+	}
+
+}
