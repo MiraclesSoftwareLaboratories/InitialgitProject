6910b8c12b28cfd0d910b006462174e4dbd10934
Miracles Software Laboratories <amit13johri@gamil.com>
2017-08-20 02:23:48 +0530

    GitHub Welcome Program

---
+++ First Java project/src/com/miraclessoftwareprivatelaboratorieschapter3/WelcomeToGit.java
@@ -0,0 +1,10 @@
+package com.miraclessoftwareprivatelaboratorieschapter3;
+//Welcome to GitHub program
+public class WelcomeToGit {
+	public static int main(String[] args) {
+		System.out.println("Hello this is the Git first program ");
+		System.out.println("It checks weather Git is working or not");
+		return 0;
+	}
+
+}
