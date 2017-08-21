5195fa51ac93a0ad9096e159a25be68b5946e785
Miracles Software Laboratories <amit13johri@gamil.com>
2017-08-21 14:50:43 +0530

    Factorial Program

---
+++ First Java project/src/com/miraclessoftwareprivatelaboratorieschapter3/FactoraiOFNumber.java
@@ -0,0 +1,24 @@
+package com.miraclessoftwareprivatelaboratorieschapter3;
+
+import java.util.Scanner;
+
+//Calculate the Factorial of the number
+public class FactoraiOFNumber {
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Please enter the number whose factorial is to find");
+		int number = keyboard.nextInt();
+		long fact=factorial(number);
+		System.out.println("The factorial "+number+" is "+fact);
+		keyboard.close();
+	}
+	public static long factorial(int num) {
+		if(num==0) {
+			return 1;
+		}
+		else 
+			return(num*factorial(num-1));
+	}
+	
+
+}
