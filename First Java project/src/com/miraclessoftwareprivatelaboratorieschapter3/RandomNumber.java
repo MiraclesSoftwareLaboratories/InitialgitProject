cbc4fee07837fc3e55104a5f39f412813f48563c
Miracles Software Laboratories <amit13johri@gamil.com>
2017-09-09 19:35:59 +0530

    Optimizes the Random Number code

--- First Java project/src/com/miraclessoftwareprivatelaboratorieschapter3/RandomNumber.java
+++ First Java project/src/com/miraclessoftwareprivatelaboratorieschapter3/RandomNumber.java
@@ -17,28 +17,10 @@ class MyException extends Exception{
 public class RandomNumber {
 	public static void main(String[] args) {
 		Random random = new Random();
-		int monkeyA=random.nextInt(50)+1;
+		int monkeyA=random.nextInt(50);
 		int monkeyB=random.nextInt(50);
 		int user=random.nextInt(50);
-		if((user>monkeyA)&&(user>monkeyB)) {
-			System.out.println("User is the Winner");
-			System.out.println("The value of user is "+user+"."+"\nThe value of MonkeyA is "+monkeyA+"."+"\nThe value of MonkeyB is "+monkeyB+".");
-		}
-		else if((monkeyA>user)&&(monkeyA>monkeyB)) {
-			System.out.println("MonkeyA is the Winner");
-			System.out.println("The value of MonkeyA is "+monkeyA+"."+"\nThe value of MonkeyB is "+monkeyB+"."+"\nThe value of user is "+user);
-			System.out.println("\n\n\n\n");
-			System.out.println("..............Warning.................");
-			System.out.println("       User loses the Game");
-		}
-		else if((monkeyB>monkeyA)&&(monkeyB>user)) {
-			System.out.println("MonkeyB is the Winner");
-			System.out.println("The value of MonkeyB is "+monkeyB+"."+"\nThe value of MonkeyA is "+monkeyA+"."+"\nThe value of user is "+user);
-			System.out.println("\n\n\n\n");
-			System.out.println("..............Warning.................");
-			System.out.println("       User loses the Game");
-		}
-		if((user==monkeyA)&&(user==monkeyB)) {
+		if((user==monkeyA)&&(user==monkeyB)&&(monkeyA==monkeyB)) {
 			try {
 				throw new MyException();
 			}
@@ -46,7 +28,20 @@ public class RandomNumber {
 				System.out.println(e);
 			}	
 		}
-	
+		if((user>monkeyA)&&(user>monkeyB)) 
+			System.out.println("User is the Winner");
+		else {
+			if(((monkeyA>user)&&(monkeyA>monkeyB))) {
+				System.out.println("       User loses the Game");
+				System.out.println("MonkeyA is the Winner");
+			}
+			else {
+				System.out.println("       User loses the Game");
+				System.out.println("MonkeyB is the Winner");
+			}
+			
+		}
+		System.out.println("The value of user is "+user+"."+"\nThe value of MonkeyA is "+monkeyA+"."+"\nThe value of MonkeyB is "+monkeyB+".");
 	}
 
 }
